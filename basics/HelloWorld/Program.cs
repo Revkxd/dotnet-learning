5895a90b8cdc84aee4ac000bad175967a6e5b1c3
Ced <79917173+Revkxd@users.noreply.github.com>
2023-06-03 07:16:49 +0800

    14 Variables

--- basics/HelloWorld/Program.cs
+++ basics/HelloWorld/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+sbyte a = 127;
+byte b = 255;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+ushort c = 65535;
+short d = -32768;
+Console.WriteLine(c);
+Console.WriteLine(d);
+
+uint e = 4294967295;
+int f = -2147483648;
+Console.WriteLine(e);
+Console.WriteLine(f);
+
+ulong g = 18446744073709551615;
+long h = -9223372036854775808;
+Console.WriteLine(g);
+Console.WriteLine(h);
+
+float i = 3.402823E+38F;
+double j = 1.7976931348623157E+308;
+Console.WriteLine(i);
+Console.WriteLine(j);
+
+decimal k = 79228162514264337593543950335M;
+Console.WriteLine(k);
+
+string l = "Hello World";
+Console.WriteLine(l);
+
+bool m = true;
+Console.WriteLine(m);
