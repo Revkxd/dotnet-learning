c5788529d4152410cb85fd1d2f1bf8120ce0c6a0
Ced <79917173+Revkxd@users.noreply.github.com>
2023-06-03 10:45:01 +0800

    25 Namespaces

---
+++ intermediate/Namespaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Namespaces.Models;
+
+namespace Namespaces {
+    internal class Program {
+        static void Main(string[] args) {
+            Computer myComputer = new Computer() {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                VideoCard = "RTX 2060"
+            };
+            Console.WriteLine(myComputer.Motherboard);
+            Console.WriteLine(myComputer.HasWifi);
+            Console.WriteLine(myComputer.ReleaseDate);
+            Console.WriteLine(myComputer.VideoCard);
+        }
+    }
+}
