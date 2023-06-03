d40d9276e2ff7348cc4fa4c6989d72f841198560
Ced <79917173+Revkxd@users.noreply.github.com>
2023-06-03 09:11:10 +0800

    18 Conditional Statements

---
+++ basics/Conditionals/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+int myInt = 5;
+int mySecondInt = 10;
+
+if(myInt < mySecondInt){
+    myInt += 10;
+}
+
+Console.WriteLine(myInt);
+
+string myCow = "cow";
+string myCapitalizedCow = "Cow";
+
+if(myCow == myCapitalizedCow){
+    Console.WriteLine("The strings are equal");
+}
+else if(myCow == myCapitalizedCow.ToLower()){
+    Console.WriteLine("Equal without case sensitivity");
+}
+else{
+    Console.WriteLine("The strings are not equal");
+}
+
+switch (myCow){
+    case "cow":
+        Console.WriteLine("Lowercase");
+        break;
+    case "Cow":
+        Console.WriteLine("Capitalized");
+        break;
+    default:
+        Console.WriteLine("Default ran");
+        break;
+}
