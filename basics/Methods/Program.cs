25d753ffaf6c5a1616c90dda6e82feddba589c55
Ced <79917173+Revkxd@users.noreply.github.com>
2023-06-03 09:54:28 +0800

    20 Methods

---
+++ basics/Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static private int GetSum(int[] intsToCompress) {
+        int totalValue = 0;
+        foreach(int intForCompression in intsToCompress)
+        {
+            totalValue += intForCompression;
+        }
+        return totalValue;
+    }
+    static void Main(string[] args)
+    {
+        int[] intsToCompress = new int[] {23, 23, 53, 56, 83, 92};
+        int sum = GetSum(intsToCompress);
+        Console.WriteLine($"The sum is {sum}");
+    }
+}
