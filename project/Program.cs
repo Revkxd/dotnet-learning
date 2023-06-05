e515b6f9073335af5ce47923199617b773c6c301
Ced <79917173+Revkxd@users.noreply.github.com>
2023-06-06 04:57:26 +0800

    31 Dapper pt1

--- project/Program.cs
+++ project/Program.cs
@@ -21,10 +21,46 @@ namespace project {
                 Price = 943.87m,
                 VideoCard = "RTX 2060"
             };
-            // Console.WriteLine(myComputer.Motherboard);
-            // Console.WriteLine(myComputer.HasWifi);
-            // Console.WriteLine(myComputer.ReleaseDate);
-            // Console.WriteLine(myComputer.VideoCard);
+
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                Motherboard,
+                HasWifi,
+                HasLTE,
+                ReleaseDate,
+                Price,
+                VideoCard
+            ) VALUES ('" + myComputer.Motherboard
+                + "', '" + myComputer.HasWifi
+                + "', '" + myComputer.HasLTE
+                + "', '" + myComputer.ReleaseDate
+                + "', '" + myComputer.Price
+                + "', '" + myComputer.VideoCard
+            + "')";
+
+            // Console.WriteLine(sql);
+            // int result = dbConnection.Execute(sql);
+            // Console.WriteLine(result);
+
+            string sqlSelect = @"SELECT
+                Computer.Motherboard,
+                Computer.HasWifi,
+                Computer.HasLTE,
+                Computer.ReleaseDate,
+                Computer.Price,
+                Computer.VideoCard
+            FROM TutorialAppSchema.Computer";
+
+            IEnumerable<Computer> computers = dbConnection.Query<Computer>(sqlSelect);
+
+            foreach (Computer computer in computers) {
+                Console.WriteLine(computer.Motherboard);
+                Console.WriteLine(computer.HasWifi);
+                Console.WriteLine(computer.HasLTE);
+                Console.WriteLine(computer.ReleaseDate);
+                Console.WriteLine(computer.Price);
+                Console.WriteLine(computer.VideoCard);
+                Console.WriteLine();
+            }
         }
     }
 }
