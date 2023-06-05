2cfd1f0e82ea8a53036604d6dc2a77ef9f66fe88
Ced <79917173+Revkxd@users.noreply.github.com>
2023-06-06 04:35:48 +0800

    29 Database connection

---
+++ project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using project.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace project {
+    public class Program {
+        static void Main(string[] args) {
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=True;Trusted_Connection=True";
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+            string sqlCommand = "SELECT GETDATE()";
+            DateTime rightnow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+            Console.WriteLine(rightnow);
+
+            Computer myComputer = new Computer() {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                VideoCard = "RTX 2060"
+            };
+            // Console.WriteLine(myComputer.Motherboard);
+            // Console.WriteLine(myComputer.HasWifi);
+            // Console.WriteLine(myComputer.ReleaseDate);
+            // Console.WriteLine(myComputer.VideoCard);
+        }
+    }
+}
