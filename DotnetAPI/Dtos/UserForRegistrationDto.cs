76de806d90e8e181922e0ff42b7e62c74714ac17
Ced <79917173+Revkxd@users.noreply.github.com>
2023-06-07 13:31:08 +0800

    73 Registration with Details

--- DotnetAPI/Dtos/UserForRegistrationDto.cs
+++ DotnetAPI/Dtos/UserForRegistrationDto.cs
@@ -2,9 +2,12 @@ namespace DotnetAPI.Dtos
 {
     public partial class UserForRegistrationDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string PasswordConfirm { get; set; }
+        public string Email {get; set;}
+        public string Password {get; set;}
+        public string PasswordConfirm {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Gender {get; set;}
 
         public UserForRegistrationDto()
         {
@@ -19,6 +22,18 @@ namespace DotnetAPI.Dtos
             if (PasswordConfirm == null)
             {
                 PasswordConfirm = "";
+            }
+            if (FirstName == null)
+            {
+                FirstName = "";
+            }
+            if (LastName == null)
+            {
+                LastName = "";
+            }
+            if (Gender == null)
+            {
+                Gender = "";
             }
         }
     }
